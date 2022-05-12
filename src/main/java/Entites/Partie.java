e202111f1577771feb8d53b357f262649e65f9d6
missdieye <khadijadieye@gmail.com>
2022-05-12 23:50:56 +0200

    parameters of class and gitignore some folder

--- src/main/java/Entites/Partie.java
+++ src/main/java/Entites/Partie.java
@@ -1,4 +1,13 @@
 package Entites;
 
 public class Partie {
+    private int idPartie;
+
+    public int getIdPartie() {
+        return idPartie;
+    }
+
+    public void setIdPartie(int idPartie) {
+        this.idPartie = idPartie;
+    }
 }
