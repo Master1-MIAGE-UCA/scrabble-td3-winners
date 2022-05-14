78428ef5030cfbdbd5e8fa5b2b2756187df08920
missdieye <khadijadieye@gmail.com>
2022-05-14 16:32:30 +0200

    test

---
+++ partie/src/test/java/com/example/partie/PartieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.partie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
