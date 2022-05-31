e8d381c22b13cd046e562eef1e19955fc7f81043
safouaneouazri <safouane1ouazri@gmail.com>
2022-05-31 15:54:14 +0200

    Create class commune

---
+++ ClassesCommune/src/Message.java
@@ -0,0 +1,27 @@
+
+public class Message {
+
+    private String message;
+
+    public Message() {
+    }
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
