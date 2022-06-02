896179c33e2defe6b863c10c3a7cb2bcccca23ec
DIALLO <fatoumata-binta.diallo@etu.univ-cotedazur.fr>
2022-06-02 15:47:33 +0200

    add traking file

---
+++ anagrammeur/src/main/java/anagrammeur/placedWord.java
@@ -0,0 +1,47 @@
+package anagrammeur;
+
+public class placedWord {
+    private String word;
+    private int abscissa;
+    private int ordinate;
+    private boolean vertical;
+
+    public placedWord(String word, int abscissa, int ordinate, boolean vertical) {
+        this.word = word;
+        this.abscissa = abscissa;
+        this.ordinate = ordinate;
+        this.vertical = vertical;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getOrdinate() {
+        return ordinate;
+    }
+
+    public boolean isVertical() {
+        return vertical;
+    }
+
+    public void setVertical(boolean vertical) {
+        this.vertical = vertical;
+    }
+
+    public void setOrdinate(int ordinate) {
+        this.ordinate = ordinate;
+    }
+
+    public int getAbscissa() {
+        return abscissa;
+    }
+
+    public void setAbscissa(int abscissa) {
+        this.abscissa = abscissa;
+    }
+}
---
+++ appariement/src/main/java/appariement/appariementHelloWorld.java
@@ -0,0 +1,10 @@
+package appariement;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class appariementHelloWorld {
+    public static void main(String[] args) {
+        SpringApplication.run(appariementHelloWorld.class, args);
+    }
+}
