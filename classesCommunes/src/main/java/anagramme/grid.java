238c5c63ece83d24249bb21dd7955d0a7ee35340
DIALLO <fatoumata-binta.diallo@etu.univ-cotedazur.fr>
2022-06-07 21:44:44 +0200

    add grid

--- classesCommunes/src/main/java/anagramme/grid.java
+++ classesCommunes/src/main/java/anagramme/grid.java
@@ -1,5 +1,29 @@
 package anagramme;
 
+
 public class grid {
 
+ box[][] grid;
+
+ public grid(box[][] grid) {
+  this.grid = new box[15][15];
+ }
+
+ public box[][] getGrid() {
+  return grid;
+ }
+
+ public void setGrid(box[][] grid) {
+  this.grid = grid;
+ }
+
+ public void init(){
+
+  for (int i = 0; i < grid.length; i++) {
+   for (int j = 0; j < grid.length; j++) {
+    this.grid[i][j] = new box(i,j);
+   }
+  }
+ }
 }
+
