d8da5db1af53318ca96357cf9337d351d1c75f42
safouaneouazri <safouane1ouazri@gmail.com>
2022-06-08 14:09:37 +0200

    Appariement fonctionnel

--- appariement/src/main/java/appariement/appariementController.java
+++ appariement/src/main/java/appariement/appariementController.java
@@ -2,22 +2,41 @@ package appariement;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 @RestController
 public class appariementController {
 
-    String URLp;
+    ArrayList<AbstractMap.SimpleEntry<String, String> > URL_Anagram_Partie_auth = new ArrayList<AbstractMap.SimpleEntry<String, String> >();
+
+    private ArrayList<String> URLjoueurs = new ArrayList<>();
+
     @PostMapping("/identification/Joueur")
-    public String identAnagrammeur() {
+    public ArrayList<AbstractMap.SimpleEntry<String, String> > identAnagrammeur() {
+        System.out.println("Appariement > un joueur est connecté ");
+if (this.URLjoueurs.size() > 1){
+    this.URL_Anagram_Partie_auth.add(new AbstractMap.SimpleEntry("CanStart", "true"));
 
-        return this.URLp;
+}
+        return this.URL_Anagram_Partie_auth;
     }
-    @PostMapping("/identification/partie/")
+    @PostMapping("/identification/partie")
     public void identPartie(@RequestBody String URL) {
-        this.URLp = URL;
+        System.out.println("Appariement > une partie est connecté ");
+        this.URL_Anagram_Partie_auth.add(new AbstractMap.SimpleEntry("URLpartie", URL));
+
 
     }
+    @PostMapping("/identification/Annagrameur")
+    public void identAnnagrameur(@RequestBody String URL) {
+        System.out.println("Appariement > un annagrameur est connecté ");
+        this.URL_Anagram_Partie_auth.add(new AbstractMap.SimpleEntry("URLanagram", URL));
+
+
+    }
+
 
 }
