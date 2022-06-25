81dbfe241eb625ac8dfc565ab4732b51f44f1f2a
DIALLO <71668834+Binta55@users.noreply.github.com>
2022-06-25 15:23:58 +0200

    update grid

--- classesCommunes/src/main/java/anagramme/placeLetter.java
+++ classesCommunes/src/main/java/anagramme/placeLetter.java
@@ -1,4 +1,24 @@
 package anagramme;
 
 public class placeLetter {
+    private  String letter;
+
+    public placeLetter(String letter){
+        this.letter = letter;
+
+    }
+
+    public String getLetter() {
+        return letter;
+    }
+
+    public void setLetter(String letter) {
+        this.letter = letter;
+    }
+    @Override
+    public String toString() {
+        return "Letter{" +
+                "letter='" + letter +
+                '}';
+    }
 }
